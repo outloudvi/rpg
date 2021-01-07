d53985aecaf2f2d1063087cd5630a34190abe24e
Outvi V <git@outv.im>
2021-01-07 15:46:33 +0800

    feat(mpmsg): seperate child function

--- mpmsg/src/main.rs
+++ mpmsg/src/main.rs
@@ -1,14 +1,19 @@
-use std::{sync::mpsc, thread::spawn};
+use std::{
+    sync::mpsc::{channel, Sender},
+    thread::spawn,
+};
+
+fn child_routine(tx: Sender<i32>) {
+    let v = 4;
+    match tx.send(v) {
+        Ok(_) => println!("Child: sent {}", v),
+        Err(e) => eprintln!("Child didn't send: {}", e),
+    };
+}
 
 fn main() {
-    let (tx, rx) = mpsc::channel();
-    let child = spawn(move || {
-        let v = 4;
-        match tx.send(v) {
-            Ok(_) => println!("Child: sent {}", v),
-            Err(e) => eprintln!("Child didn't send: {}", e),
-        };
-    });
+    let (tx, rx) = channel::<i32>();
+    let child = spawn(move || child_routine(tx));
     println!("Parent: recv {}", rx.recv().unwrap());
     child.join().unwrap();
 }
