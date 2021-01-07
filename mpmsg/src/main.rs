1299ddda7b5f0ddb7b7b8a25418164be994cc475
Outvi V <git@outv.im>
2021-01-07 16:08:28 +0800

    feat(mpmsg): multi children

--- mpmsg/src/main.rs
+++ mpmsg/src/main.rs
@@ -3,17 +3,30 @@ use std::{
     thread::spawn,
 };
 
-fn child_routine(tx: Sender<i32>) {
-    let v = 4;
-    match tx.send(v) {
-        Ok(_) => println!("Child: sent {}", v),
-        Err(e) => eprintln!("Child didn't send: {}", e),
-    };
+fn child_routine(tx: Sender<i32>, start: i32, step: usize) {
+    for v in start..start + step as i32 {
+        match tx.send(v) {
+            Ok(_) => println!("Child: sent {}", v),
+            Err(e) => panic!("Child didn't send: {}", e),
+        };
+    }
 }
 
 fn main() {
     let (tx, rx) = channel::<i32>();
-    let child = spawn(move || child_routine(tx));
-    println!("Parent: recv {}", rx.recv().unwrap());
-    child.join().unwrap();
+    let tx1 = Sender::clone(&tx);
+    let tx2 = Sender::clone(&tx);
+    drop(tx); // ... or the program will not end
+    let child1 = spawn(move || child_routine(tx1, 1, 5));
+    let child2 = spawn(move || child_routine(tx2, 7, 5));
+    loop {
+        match rx.recv() {
+            Ok(v) => println!("Parent: recv {}", v),
+            Err(_) => break,
+        }
+    }
+
+    for i in vec![child1, child2].drain(..) {
+        i.join().unwrap();
+    }
 }
